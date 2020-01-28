5a8d96680c651d9537ef8404a995a60871a3f9d7
Uday Thota <udaydeep.thota@gmail.com>
2020-01-27 23:52:39 -0800

    add the solution for finding leaves of the bt

---
+++ src/main/java/med/_366_Find_Leaves_Of_BinaryTree.java
@@ -0,0 +1,71 @@
+package med;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by udaythota on 1/27/20.
+ * <p>
+ * Given a binary tree, collect a tree's nodes as if you were doing this: Collect and remove all leaves, repeat until the tree is empty.
+ * </p>
+ */
+public class _366_Find_Leaves_Of_BinaryTree {
+    // core logic: DFS solution: find the leaf nodes and then tell its parent to delete the leaves. the boolean value is to tell parent node to whether delete the child node or not
+    private static List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        while (root != null) {
+            List<Integer> layer = new ArrayList<>();
+            if (removeLeaves(root, layer)) {
+                root = null;   // as both the children of this node are removed, now remove the parent node (make it null)
+            }
+            result.add(layer);
+        }
+        return result;
+    }
+
+    private static boolean removeLeaves(TreeNode root, List<Integer> layer) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            layer.add(root.val);
+            return true;
+        }
+
+        if (removeLeaves(root.left, layer)) {
+            root.left = null;
+        }
+
+        if (removeLeaves(root.right, layer)) {
+            root.right = null;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>(Arrays.asList(4, 5, 3)));
+        expected.add(new ArrayList<>(Arrays.asList(2)));
+        expected.add(new ArrayList<>(Arrays.asList(1)));
+        assertEquals(findLeaves(root), expected);
+    }
+}
