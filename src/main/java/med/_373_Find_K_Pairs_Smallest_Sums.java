87e261c992cb4f62b845a793bd00da6377eeeccc
Uday Thota <udaydeep.thota@gmail.com>
2020-01-29 10:26:20 -0800

    draft for k smallest sums

---
+++ src/main/java/med/_373_Find_K_Pairs_Smallest_Sums.java
@@ -0,0 +1,21 @@
+package med;
+
+import java.util.List;
+
+/**
+ * Created by udaythota on 1/29/20.
+ * <p>
+ * You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+ * Define a pair (u,v) which consists of one element from the first array and one element from the second array.
+ * Find the k pairs (u1,v1),(u2,v2) ...(uk,vk) with the smallest sums.
+ * </p>
+ */
+public class _373_Find_K_Pairs_Smallest_Sums {
+    private static List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
