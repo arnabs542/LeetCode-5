671a94ed37cc2f4fddb3dd5ff3206699a1c5e24e
z002jsf <udaydeep.thota@target.com>
2020-02-25 15:32:46 -0800

    add the 2nd solution for knight probability chess board

--- src/main/java/med/_688_Knight_Probability_Chess_Board.java
+++ src/main/java/med/_688_Knight_Probability_Chess_Board.java
@@ -11,6 +11,10 @@ import static org.testng.Assert.assertEquals;
  * </p>
  */
 public class _688_Knight_Probability_Chess_Board {
+    // for every point on the grid, navigate all the 8 directions and calculate the probabilities accordingly. repeat this process K times.
+    // remember when the point on the grid is visited again (eg: for the 2nd time), its probability will become existing value / 8 and this continues..
+    // we start the process by setting the probability to 1 at the given row and column. in the first iteration of K, all 8 valid (which still falls on board after the move) points from which we can move from given x and y will be set to probability of 1/8. in the next iteration of K, from each of these new points, we again move 8 valid points and adjust the probability as needed
+    // TC: O(k * n^2), SC: O(k * n^2)
     private static double knightProbability(int N, int K, int r, int c) {
         int[][] directions = new int[][]{{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};   // all the possible 8 directions a knight can move from a given position
         double[][][] dp = new double[K + 1][N][N];    // dp array to remember the probabilities
@@ -39,7 +43,40 @@ public class _688_Knight_Probability_Chess_Board {
         return totalProbability;
     }
 
+    // core logic: same as above but we are optimizing for space here, as we only need k-1th level probability values when calculating the probabilities at kth level. uses a temp dp array to calculate the probabilities at kth level and then assign it back to the actual dp array so it can be used in next to be computed levels.
+    // TC: O(k * n^2), SC: O(n^2)
+    private static double knightProbability2(int N, int K, int r, int c) {
+        int[][] directions = new int[][]{{-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};   // all the possible 8 directions a knight can move from a given position
+        double[][] dp = new double[N][N];    // dp array to remember the probabilities
+        dp[r][c] = 1.0;
+
+        for (int currentMove = 1; currentMove <= K; currentMove++) {
+            double[][] tempDp = new double[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    for (int[] direction : directions) {
+                        int newRow = i + direction[0];
+                        int newColumn = j + direction[1];
+                        if (newRow >= 0 && newColumn >= 0 && newRow < N && newColumn < N) {   // valid move: the new move position should still be on the board
+                            tempDp[i][j] += dp[newRow][newColumn] / 8.0;
+                        }
+                    }
+                }
+            }
+            dp = tempDp;
+        }
+
+        double totalProbability = 0.0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                totalProbability += dp[i][j];
+            }
+        }
+        return totalProbability;
+    }
+
     public static void main(String[] args) {
         assertEquals(knightProbability(3, 2, 0, 0), 0.0625);
+        assertEquals(knightProbability2(3, 2, 0, 0), 0.0625);
     }
 }
